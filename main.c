720d9aa730067729a5f384ad4303dfd2f6c93aee
Aleksey Pavliuk <pavliuk.aleksey@gmail.com>
2018-09-01 21:38:59 +0300

    total of ls

--- main.c
+++ main.c
@@ -15,10 +15,14 @@
 int		main(int argc, char **argv)
 {
 	t_ls *ls;
+	char *pwd;
+	char *tmp;
+	unsigned int total;
 
 	ls = (t_ls *)malloc(sizeof(t_ls));
 	ls->stat = (struct stat *)malloc(sizeof(struct stat));
 	ls->dir = (struct dirent *)malloc(sizeof(struct dirent));
+	total = 0;
 	if (argc > 1)
 	{
 		ls->fd_dir = opendir(argv[1]);
@@ -26,14 +30,23 @@ int		main(int argc, char **argv)
 		{
 			ft_printf("=========================\n");
 			ft_printf("[{green}  %s   {eoc}]\n", ls->dir->d_name);
-			stat(ls->dir->d_name, ls->stat);
+			pwd = ft_strjoin(argv[1], "/");
+			tmp = pwd;
+			pwd = ft_strjoin(pwd, ls->dir->d_name);
+			free(tmp);
+			stat(pwd, ls->stat); // ERROR!!! отправлять путь к файлу, а не имя файла!!!!
+			free(pwd);
 			ft_printf("id: %d\n", ls->stat->st_dev);
 			ft_printf("inode: %d\n", ls->stat->st_ino);
 			ft_printf("uid: %d\n", ls->stat->st_uid);
 			ft_printf("mode: %lld\n", ls->stat->st_mode);
 			ft_printf("size: %lld\n", ls->stat->st_size);
+			ft_printf("blocks: %lld\n", ls->stat->st_blocks);
+			total += ls->stat->st_blocks;
 			ft_bzero(ls->stat, sizeof(*ls->stat));
 		}
+		ft_printf("=========================\n");
+		ft_printf("total: %u\n", total);
 	}
 	else
 		ft_printf("OK!\n");
